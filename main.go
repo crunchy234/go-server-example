14d0fbacb1a33d662b99a92bc9587f065b972df7
crunchy234 <6490077+crunchy234@users.noreply.github.com>
2025-02-24 11:45:59 +1300

    feat: uses multiplexing for server
    Also handles request query params

--- main.go
+++ main.go
@@ -11,12 +11,25 @@ func getHealth(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(200)
 }
 
+func getHello(w http.ResponseWriter, r *http.Request) {
+	var name string
+	if r.URL.Query().Has("name") {
+		name = r.URL.Query().Get("name")
+	} else {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s\n", name)
+}
+
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func main() {
-	http.HandleFunc("/health", getHealth)
-	err := http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", getHealth)
+	mux.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":8080", mux)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Println("Server closed")
 	} else if err != nil {
